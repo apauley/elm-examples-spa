f88dcea71d34cc7171a2a9776a165beb35ed568e
Andreas Pauley <andreas@pauley.org.za>
2024-01-14 20:19:37 +0100

    Update Home page text

--- src/Page/Home.elm
+++ src/Page/Home.elm
@@ -1,11 +1,29 @@
 module Page.Home exposing (view)
 
-import Element exposing (..)
-import Framework.Group exposing (center)
+import Element as E exposing (Element)
+import Framework.Heading as Heading
+import UI
 
 
 view : ( String, Element msg )
 view =
-    ( "Home sweet home"
-    , el [ padding 200 ] <| text "This is the home page. Click a link."
+    ( "Home"
+    , E.column [ E.width E.fill ]
+        [ E.el (Heading.h1 ++ [ E.centerX ]) <| E.text "Elm Examples"
+        , E.paragraph []
+            [ E.text "An Elm single-page application with running examples from "
+            , UI.externalLink "https://elm-lang.org/examples" "elm-lang.org"
+            , E.text "."
+            ]
+        , E.paragraph []
+            [ E.text "The structure is mostly based on "
+            , UI.externalLink "https://github.com/rtfeldman/elm-spa-example/tree/master" "Richard Feldman's single-page example"
+            , E.text "."
+            ]
+        , E.paragraph []
+            [ E.text "Styling is done using "
+            , UI.externalLink "https://package.elm-lang.org/packages/mdgriffith/elm-ui/latest/" "elm-ui"
+            , E.text "."
+            ]
+        ]
     )
